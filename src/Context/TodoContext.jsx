fb6e2ca4a6d0d50d55c3b5be9352cb1be5e8876f
manish-bnt <manishbasnet500@gmail.com>
2025-09-03 10:29:19 +0530

    Initital commit-readt todolist

---
+++ src/Context/TodoContext.jsx
@@ -0,0 +1,82 @@
+import { createContext, useState } from "react";
+
+export const TodoContext = createContext();
+
+export function TodoProvider({ children }) {
+  const [todo, setTodo] = useState(
+    JSON.parse(localStorage.getItem("userTodoList")) || []
+  );
+  const [add, setAdd] = useState(false); // floating add button
+  const [editId, setEditId] = useState(null); // access index by clicking edit button
+  const [edit, setEdit] = useState(false); // toggle edit component
+  const [openMenu,setOpenMenu] = useState(false)
+   const [userEdit, setUserEdit] = useState({
+    title: "",
+    desc: "",
+  });
+  
+  const menuHandler=(e)=> {
+    e.stopPropagation(); 
+    setOpenMenu((prev) => !prev)
+  }
+
+  
+  const editHandler = (id) => {
+    setEdit((prev) => !prev);
+    setUserEdit(todo[id])
+    setEditId(id);
+  };
+
+  const deleteHandler = (id) => {
+    const updateTodo = todo.toSpliced(id, 1);
+    localStorage.setItem("userTodoList", JSON.stringify(updateTodo));
+    setTodo(updateTodo);
+  };
+  const addHandler = () => {
+    setAdd((prev) => !prev);
+    console.log("hi i am activate");
+  };
+
+  const favHandler = (index) => {
+    const matchedUser = todo.find((val, idx) => idx === index);
+    console.log(index);
+    console.log(matchedUser);
+    if (matchedUser) {
+      const favouriteUpdation = { ...matchedUser, isFav: !matchedUser.isFav };
+      const todoUpdate = todo.toSpliced(index, 1, favouriteUpdation);
+      setTodo(todoUpdate);
+      console.log(favouriteUpdation);
+      console.log(todoUpdate);
+
+      localStorage.setItem("userTodoList", JSON.stringify(todoUpdate));
+    }
+  };
+
+
+
+  return (
+    <TodoContext.Provider
+      value={{
+        todo,
+        setTodo,
+        add,
+        setAdd,
+        addHandler,
+        edit,
+        setEdit,
+        editHandler,
+        editId,
+        deleteHandler,
+        favHandler,
+        openMenu,
+        setOpenMenu,
+        menuHandler,
+        userEdit,
+        setUserEdit,
+      }}
+    >
+
+      {children}
+    </TodoContext.Provider>
+  );
+}
